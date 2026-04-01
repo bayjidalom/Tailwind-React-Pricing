c567640cd5fc3d50a69b480cb5dfd84e6ff0c4e8
bayjidalom <bayjidalom1005@gmail.com>
2026-04-01 12:31:17 +0600

    Daisy Navbar added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul className='flex '>
+                <li className='mr-10 font-bold'><a href="">Anchor - 1</a></li>
+                <li className='mr-10 font-bold'><a href="">Anchor - 2</a></li>
+                <li className='mr-10 font-bold'><a href="">Anchor - 3</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
